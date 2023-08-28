c7c652a7bee55d2431e2cdf23ced471c18774226
dannyspanish <d@nnyclark.com>
2023-08-28 23:23:56 +0100

    Update spa.blade.php

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -22,6 +22,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
   @vite('resources/js/app.js')
 
